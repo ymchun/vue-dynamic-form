42e33803989e25c4f3c4e10e3bc22d18db5d2bac
John Yip <john@heyavo.com>
2020-07-08 18:10:22 +0800

    fix: register components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import Vue from 'vue'
+
 import ColumnSectionItem from './components/column-section-item.vue'
 import DynamicForm from './components/dynamic-form.vue'
 import RowSectionItem from './components/row-section-item.vue'
+
+Vue.component('ColumnSectionItem', ColumnSectionItem)
+Vue.component('DynamicForm', DynamicForm)
+Vue.component('RowSectionItem', RowSectionItem)
 
 export { ColumnSectionItem, DynamicForm, RowSectionItem }
 export * from './types/dynamic-form'
