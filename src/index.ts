bb35c2c3cac4e4c5a85d438cf6b7576933cd46d3
Jean M.C <manchunyip2008@gmail.com>
2020-07-08 20:36:51 +0800

    fix: change to plugin mode

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import Vue from 'vue'
+import { VueConstructor } from 'vue'
 
 import ColumnSectionItem from './components/column-section-item.vue'
 import DynamicForm from './components/dynamic-form.vue'
 import RowSectionItem from './components/row-section-item.vue'
 
-Vue.component('ColumnSectionItem', ColumnSectionItem)
-Vue.component('DynamicForm', DynamicForm)
-Vue.component('RowSectionItem', RowSectionItem)
-
-export { ColumnSectionItem, DynamicForm, RowSectionItem }
+export default {
+  install(_vue: VueConstructor) {
+    _vue.component('ColumnSectionItem', ColumnSectionItem)
+    _vue.component('DynamicForm', DynamicForm)
+    _vue.component('RowSectionItem', RowSectionItem)
+  },
+}
 export * from './types/dynamic-form'
